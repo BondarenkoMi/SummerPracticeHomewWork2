5445091b784344d7e7dadf8f6588d94a4c2fb308
Bondarenko Mikhail <mihail.bondarenko.2006@mail.ru>
2024-07-02 05:10:21 +0300

    im tired

---
+++ app/src/main/java/ru/itis/homework2/fragments/MusicFragment.kt
@@ -0,0 +1,31 @@
+package ru.itis.homework2.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ru.itis.homework2.databinding.FragmentMusicBinding
+
+open class MusicFragment : Fragment() {
+    private var binding: FragmentMusicBinding? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentMusicBinding.bind(view)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentMusicBinding.inflate(inflater, container, false)
+        return binding!!.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
+}
